e298fbe5ecb600b2cc0e4704ed0a8198affde3e3
seoyun <240901@module-link.com>
2025-09-23 18:12:48 +0900

    setting: 시스템 ui 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cake/config/di.dart';
 import 'package:cake/my_app.dart';
+import 'package:cake/config/color_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -11,6 +12,19 @@ void main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+
+  // 시스템 UI(상태바/내비게이션바) 스타일 설정
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      // statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: ColorConfig.bottomNavi,
+      systemNavigationBarIconBrightness: Brightness.dark,
+      systemNavigationBarDividerColor: Colors.transparent,
+      // systemStatusBarContrastEnforced: false,
+      systemNavigationBarContrastEnforced: false,
+    ),
+  );
 
   await diSetup();
 
