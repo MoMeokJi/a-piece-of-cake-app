03627419c2e8aa533f53e98a030d411eaadb50af
seoyun <nuyoes1618@gmail.com>
2026-03-26 18:43:50 +0900

    version: android v1.1.0/ ios v1.0.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.momeokji.cake"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.1.0"
     }
 
     signingConfigs {
