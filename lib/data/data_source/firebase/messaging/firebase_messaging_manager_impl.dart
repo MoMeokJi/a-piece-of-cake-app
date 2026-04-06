331af8526c014e464ea6c87ec646f9e04d2d62bd
seoyun <nuyoes1618@gmail.com>
2026-04-06 20:15:55 +0900

    fix: fcm토큰 못받아왔을때 서버에 null로 가지 않도록 uuid 방어코드 구현

--- lib/data/data_source/firebase/messaging/firebase_messaging_manager_impl.dart
+++ lib/data/data_source/firebase/messaging/firebase_messaging_manager_impl.dart
@@ -2,33 +2,37 @@ import 'dart:io';
 import 'package:cake/data/data_source/firebase/messaging/firebase_messaging_manager.dart';
 import 'package:cake/utils/app_logger.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:uuid/uuid.dart';
 
 class FirebaseMessagingManagerImpl implements FirebaseMessagingManager {
   final FirebaseMessaging _messaging;
 
   FirebaseMessagingManagerImpl() : _messaging = FirebaseMessaging.instance;
+@override
+Future<String> getToken() async {
+  if (Platform.isIOS) {
+    AppLogger.log('iOS에서 APNS 토큰 확인 중...');
+    final apnsToken = await _messaging.getAPNSToken();
 
-  @override
-  Future<String> getToken() async {
-    // iOS에서만 APNS 토큰 체크
-    if (Platform.isIOS) {
-      AppLogger.log('iOS에서 APNS 토큰 확인 중...');
-      final apnsToken = await _messaging.getAPNSToken();
-
-      if (apnsToken == null) {
-        AppLogger.log('APNS 토큰 없음, 2초 대기 후 재시도');
-        await Future.delayed(Duration(seconds: 2));
-      } else {
-        AppLogger.log('APNS 토큰 확보됨');
-      }
+    if (apnsToken == null) {
+      AppLogger.log('APNS 토큰 없음, 2초 대기 후 재시도');
+      await Future.delayed(Duration(seconds: 2));
+    } else {
+      AppLogger.log('APNS 토큰 확보됨');
     }
-
-    AppLogger.log('FCM 토큰 요청 중...');
-    final token = await _messaging.getToken();
-    if (token == null) throw Exception('FCM token is null');
-    AppLogger.log('FCM token : $token');
-    return token;
   }
+
+  AppLogger.log('FCM 토큰 요청 중...');
+  final token = await _messaging.getToken();
+  
+  if (token == null) {
+    AppLogger.log('FCM token null, UUID로 대체');
+    return const Uuid().v4();
+  }
+  
+  AppLogger.log('FCM token : $token');
+  return token;
+}
 
   @override
   Future<NotificationSettings> requestPermission() async {
